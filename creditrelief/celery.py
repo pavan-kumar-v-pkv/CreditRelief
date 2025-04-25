0d45d7ff9eee489db3479f3fd16c91c576345192
pkv-woodstock <pkvstarscream@gmail.com>
2025-04-25 21:05:05 +0530

    Initial setup: Django + Celery + Redis

---
+++ creditrelief/celery.py
@@ -0,0 +1,11 @@
+"""Celery configuration for CreditRelief project."""
+
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'creditreflief.settings')
+
+app = Celery('creditrelief')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
